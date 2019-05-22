6f8d42b2d42d73ceaeac1c86c5c45e3012df5145
jiangtao <tao.jiang@sunyard.com>
2019-05-22 09:36:11 +0800

    初始化项目

---
+++ pmc-webapp/src/main/java/com/dvcn/pmc/PmcWebappApplication.java
@@ -0,0 +1,13 @@
+package com.dvcn.pmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmcWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmcWebappApplication.class, args);
+	}
+
+}
