a6d89c7929addf36d570bb0d26ca5af8dbb8608f
jiangtao <tao.jiang@sunyard.com>
2019-05-22 21:11:45 +0800

    初始化项目

--- pmc-webapp/src/main/java/com/dvcn/pmc/PmcWebappApplication.java
+++ pmc-webapp/src/main/java/com/dvcn/pmc/PmcWebappApplication.java
@@ -2,8 +2,10 @@ package com.dvcn.pmc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PmcWebappApplication {
 
 	public static void main(String[] args) {
